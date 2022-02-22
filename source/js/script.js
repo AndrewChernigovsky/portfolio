5b00c866c3c95050c4ca8ddd85174eed8ca0c677
Andrew Chernigovsky <chernigovsky108@gmail.com>
2022-02-22 19:41:15 +0300

    1

--- source/js/script.js
+++ source/js/script.js
@@ -17,40 +17,17 @@ navToggle.addEventListener('click', function() {
 });
 
 // Form
-const 
-  mainForm = document.getElementById('mainForm'),
-  userNameMainForm = document.getElementById('username'),
-  userPhoneMainForm = document.getElementById('phone'),
-  buttonMainForm = document.getElementById('mainFormSubmit');
+const anchors = document.querySelectorAll('a[href*="#"]')
 
-  mainForm.addEventListener('submit', function(evt) {
-        if (isValidName(userNameMainForm)){
-          userNameMainForm.classList.add('error')
-          evt.preventDefault();
-        } else {
-          userNameMainForm.classList.remove('error')
-          userNameMainForm.classList.add('success');
-      }  
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
 
-      if (isValidPhone(userPhoneMainForm)){
-        userPhoneMainForm.classList.add('error')
-        evt.preventDefault();    
-      } else {
-        userPhoneMainForm.classList.remove('error')
-        userPhoneMainForm.classList.add('success');
-        
-    }  
-  });
+    const blockID = anchor.getAttribute('href').substr(1)
 
-  function isValidPhone(input){
-    return !/^(\+7|7|8)?[\s\-]?\(?[489][0-9]{2}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2}$/.test(input.value);
-  };
-  
-  function isValidName(input){
-    return !/^\D{1,3}[А-Яа-яA-Za-z0-9_-]{3,30}$/.test(input.value);
-  };
-
-userPhoneMainForm.oninput = function(){
-  var rep = /[a-zA-ZА-Яа-я]/g; 
-  this.value = this.value.replace(rep, '');
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
 }
